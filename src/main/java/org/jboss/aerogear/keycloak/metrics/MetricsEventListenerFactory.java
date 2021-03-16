e250aeb0f60e8ebc12b0a737aba8851f22732069
Nickolas Heckman <nickolas@afero.io>
2021-03-16 09:44:45 -0700

    Read realms via session

--- src/main/java/org/jboss/aerogear/keycloak/metrics/MetricsEventListenerFactory.java
+++ src/main/java/org/jboss/aerogear/keycloak/metrics/MetricsEventListenerFactory.java
@@ -5,10 +5,6 @@ import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventListenerProviderFactory;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
-import org.keycloak.models.RealmProvider;
-import org.keycloak.models.RealmProviderFactory;
-import org.keycloak.provider.ProviderFactory;
-import org.keycloak.services.resources.RealmsResource;
 
 public class MetricsEventListenerFactory implements EventListenerProviderFactory {
 
@@ -28,15 +24,9 @@ public class MetricsEventListenerFactory implements EventListenerProviderFactory
     @Override
     public void postInit(KeycloakSessionFactory factory) {
         KeycloakSession session = factory.create();
-        session.getTransactionManager().begin();
-
-        ProviderFactory<RealmProvider> providerFactory = factory.getProviderFactory(RealmProvider.class);
-        RealmProvider realmProvider = providerFactory.create(session);
-        realmProvider.getRealmsStream().forEach(model -> metricsEventListener.initRealm(model.getId()));
+        session.realms().getRealmsStream().forEach(model -> metricsEventListener.initRealm(model.getId()));
 
         // TODO: Init the clients and providers
-
-        session.getTransactionManager().commit();
     }
 
     @Override
